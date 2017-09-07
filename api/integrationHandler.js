cd13f2279c0675236a9772f4f4cbe0df8ebf125b
CreativeGuy2013 <lucacasonato@yahoo.com>
2017-09-07 10:24:17 +0200

    Made some layout coolness

--- api/integrationHandler.js
+++ api/integrationHandler.js
@@ -11,34 +11,56 @@ function integrations(language, tutorialID, tagID) {
         codeRealtime: {
             web: function(interactiveFile, html_lock, css_lock, js_lock) {
                 loadScriptRedirect("tutorials/" + language + "/" + tutorialID + "/interactive/code_realtime/" + interactiveFile + ".tcst", function() {
-                    document.getElementById(tagID).innerHTML = '<table width="800px" height="600px" style=" border: 1px solid black;">\
+                    document.getElementById(tagID).innerHTML = '<table class="interactiveTable2">\
                         <col width="50%">\
                         <col width="50%">\
                         <tr style="height: 50%;">\
                             <td>\
                                 <p>HTML</p>\
-                                <div onkeyup="integrationsProcessor(' + interactive.id + ').codeRealtime();" id="html_' + interactive.id + '" style="width:390px;height:290px;border:1px solid black"></div>\
+                                <div onkeyup="integrationsProcessor(' + interactive.id + ').codeRealtime.web();" id="html_' + interactive.id + '" class="webLayoutFrames" style="border:1px solid black"></div>\
                             </td>\
                             <td>\
                                 <p>CSS</p>\
-                                <div onkeyup="integrationsProcessor(' + interactive.id + ').codeRealtime();" id="css_' + interactive.id + '" style="width:390px;height:290px;border:1px solid black"></div> \
+                                <div onkeyup="integrationsProcessor(' + interactive.id + ').codeRealtime.web();" id="css_' + interactive.id + '" class="webLayoutFrames" style="border:1px solid black"></div> \
                             </td>\
                         </tr>\
                         <tr style="height: 50%;">\
                             <td>\
                                 <p>JS</p>\
-                                <div onkeyup="integrationsProcessor(' + interactive.id + ').codeRealtime();" id="js_' + interactive.id + '" style="width:390px;height:290px;border:1px solid black"></div> \
+                                <div onkeyup="integrationsProcessor(' + interactive.id + ').codeRealtime.web();" id="js_' + interactive.id + '" class="webLayoutFrames" style="border:1px solid black"></div> \
                             </td>\
                             <td>\
                                 <p>Result</p>\
-                                <iframe id="result_' + interactive.id + '" style="width:390px;height:290px;border:1px solid black"></iframe> \
+                                <iframe id="result_' + interactive.id + '" class="webLayoutFrames" style="border:1px solid black; background: white;"></iframe> \
                             </td>\
                         </tr>\
                     </table>';
                     
                     createEditor("html_" + interactive.id, interactive.defaults.html, "html", html_lock);
                     createEditor("css_" + interactive.id, interactive.defaults.css, "css", css_lock);
-                    createEditorCallback("js_" + interactive.id, interactive.defaults.js, "javascript", js_lock, "integrationsProcessor(" + interactive.id + ").codeRealtime();");
+                    createEditorCallback("js_" + interactive.id, interactive.defaults.js, "javascript", js_lock, "integrationsProcessor(" + interactive.id + ").codeRealtime.web();");
+
+                });
+                
+            },
+            html: function(interactiveFile, html_lock) {
+                loadScriptRedirect("tutorials/" + language + "/" + tutorialID + "/interactive/code_realtime/" + interactiveFile + ".tcst", function() {
+                    document.getElementById(tagID).innerHTML = '<table class="interactiveTable1">\
+                        <col width="50%">\
+                        <col width="50%">\
+                        <tr style="height: 50%;">\
+                            <td>\
+                                <p>HTML</p>\
+                                <div onkeyup="integrationsProcessor(' + interactive.id + ').codeRealtime.html();" id="html_' + interactive.id + '" class="webLayoutFrames" style="border:1px solid black"></div>\
+                            </td>\
+                            <td>\
+                                <p>Result</p>\
+                                <iframe id="result_' + interactive.id + '" class="webLayoutFrames" style="border:1px solid black; background: white;"></iframe> \
+                            </td>\
+                        </tr>\
+                    </table>';
+                    
+                    createEditorCallback("html_" + interactive.id, interactive.defaults.html, "html", html_lock, "integrationsProcessor(" + interactive.id + ").codeRealtime.html();");
 
                 });
                 
@@ -63,8 +85,9 @@ function integrations(language, tutorialID, tagID) {
 
 function integrationsProcessor(interactiveID) {
     return {
-        codeRealtime: function() {
-            var html = document.getElementById("var__html_" + interactiveID);
+        codeRealtime: {
+            web: function() {
+                var html = document.getElementById("var__html_" + interactiveID);
                 var css = document.getElementById("var__css_" + interactiveID);
                 var js = document.getElementById("var__js_" + interactiveID);
                 var result = document.getElementById("result_" + interactiveID);
@@ -76,7 +99,16 @@ function integrationsProcessor(interactiveID) {
                 style.innerHTML = monaco.editor.getModels()[css.innerHTML - 1].getValue();
                 script.innerHTML = monaco.editor.getModels()[js.innerHTML - 1].getValue();
                 iframe.head.appendChild(style);
-    			iframe.head.appendChild(script);
-        } 
-    }
+                iframe.head.appendChild(script);
+            },
+            html: function() {
+                var html = document.getElementById("var__html_" + interactiveID);
+                var result = document.getElementById("result_" + interactiveID);
+                var iframe = result.contentDocument;
+                
+                result.contentDocument.getElementsByTagName("html")[0].innerHTML = monaco.editor.getModels()[html.innerHTML - 1].getValue();
+
+            }
+        }
+        }   
 }
